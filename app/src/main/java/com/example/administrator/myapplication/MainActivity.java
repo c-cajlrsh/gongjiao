3143f6f78e5458540442fb19cf559b209060ba08
c-cajlrsh <shi69228008@163.com>
2021-05-06 15:05:39 +0800

    小董

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.administrator.myapplication;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      new Handler().postDelayed(new Runnable() {
+          @Override
+          public void run() {
+              SharedPreferences sp = MainActivity.this.getSharedPreferences("apkifo", MODE_APPEND);
+              String ste  = sp.getString("diyi"," ");
+              if (ste.equals("1")){
+                  Intent intent = new Intent(MainActivity.this, ZhuJianMian.class);
+                  startActivity(intent);
+                  finish();
+              }else {
+                  diao();
+              }
+          }
+      },2000);
+
+
+    }
+
+    public void chun(){
+        SharedPreferences apkifo = MainActivity.this.getSharedPreferences("apkifo", MODE_APPEND);
+        SharedPreferences.Editor dp = apkifo.edit();
+        dp.putString("diyi","1");
+        dp.commit();
+    }
+
+    public void diao(){
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("本数据来自于聚合数据平台，仅供参考\n你确定使用吗？");
+                builder.setPositiveButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.exit(0);
+                    }
+                });
+                builder.setNegativeButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(MainActivity.this, ZhuJianMian.class);
+                        startActivity(intent);
+                        chun();
+                        finish();
+                    }
+                });
+                builder.show();
+    }
+
+
+}
