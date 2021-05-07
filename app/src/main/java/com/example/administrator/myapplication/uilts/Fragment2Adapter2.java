f39c8657e844575e90ea404bbc395a889ed13525
c-cajlrsh <shi69228008@163.com>
2021-05-07 17:20:38 +0800

    小董

--- app/src/main/java/com/example/administrator/myapplication/uilts/Fragment2Adapter2.java
+++ app/src/main/java/com/example/administrator/myapplication/uilts/Fragment2Adapter2.java
@@ -4,23 +4,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.example.administrator.myapplication.R;
+
 /**
  * Created by Administrator on 2017/1/4.
  */
 public class Fragment2Adapter2 extends BaseAdapter {
+    String names[] = {"星座运势","历史的今天","微信精选","驾照题库"};
+    int tu [] ={R.drawable.xinzuo,R.drawable.lishi,R.drawable.weixin,R.drawable.jiazaho};
     @Override
     public int getCount() {
-        return 0;
+        return names.length;
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return names[position];
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
